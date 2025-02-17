8062c75bffc4fd029cd300b48895897476615c15
Your Name <elouan.duranton.mamadou@gmail.com>
2025-02-17 15:47:58 +0100

    new branch starting jobs

--- src/handlers.c
+++ src/handlers.c
@@ -1,11 +1,10 @@
 #include "handlers.h"
 
+/* Handler for the SIGCHILD signal */
 void handler_sigchild(int sig)
 {
     pid_t pid;
-    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
-    	// printf("Handler reaped child %d\n", (int)pid);
-    }
+    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0)
     if (pid == -1 && errno != ECHILD) { // a bien comprendre le errno -> no more children left -> with ECHILD is not a ERROR
     	unix_error("waitpid error");
     }
